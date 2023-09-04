cdc7775d2e25697c8fd7bf2f5227bdb2cfa8c299
Josh Merlin <jmerlin.suf@gmail.com>
2023-09-04 09:51:49 +0800

    client: api url env file

--- client/src/api/notes.ts
+++ client/src/api/notes.ts
@@ -1,17 +1,19 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/notes";
+const API_URL = import.meta.env.API_URL || "http://localhost:8000";
+
+const NOTES_API_URL = `${API_URL}/notes`;
 
 export const fetchNotes = async () => {
-  return axios(`${API_URL}`);
+  return axios(`${NOTES_API_URL}`);
 };
 
 export const createNote = async (data?: { title?: string; note?: string }) => {
-  return axios.post(`${API_URL}`, data);
+  return axios.post(`${NOTES_API_URL}`, data);
 };
 
 export const deleteNote = async (id: number) => {
-  return axios.delete(`${API_URL}/${id}`);
+  return axios.delete(`${NOTES_API_URL}/${id}`);
 };
 
 export const updateNote = async ({
@@ -23,7 +25,7 @@ export const updateNote = async ({
   title?: string;
   note?: string;
 }) => {
-  return axios.post(`${API_URL}`, { id, title, note });
+  return axios.post(`${NOTES_API_URL}`, { id, title, note });
 };
 
 export const batchUpdate = async (
@@ -34,7 +36,7 @@ export const batchUpdate = async (
     id: key,
   }));
 
-  return axios.put(`${API_URL}/batch`, data, {
+  return axios.put(`${NOTES_API_URL}/batch`, data, {
     params: {
       batch: true,
     },
