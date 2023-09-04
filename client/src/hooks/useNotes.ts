af0e77dae8ae93b6124afcf54d7980e4fff81169
Josh Merlin <jmerlin.suf@gmail.com>
2023-09-04 09:27:20 +0800

    client: useNotes commit

--- client/src/hooks/useNotes.ts
+++ client/src/hooks/useNotes.ts
@@ -1,65 +1,116 @@
-import { create } from "zustand";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  createNote as addNoteAPI,
+  fetchNotes as getNotesAPI,
+  deleteNote as deleteNoteAPI,
+  batchUpdate,
+} from "../api/notes";
+import { useCallback, useRef } from "react";
 
-type NoteID = string | number;
+type NoteID = number;
 // type NoteGroupID = string | number;
 
-export interface Note {
+export interface NoteContent {
+  title?: string;
+  note?: string;
+}
+
+export interface Note extends NoteContent {
   id: NoteID;
-  title: string;
-  note: string;
 }
 
-// interface NoteGroup {
-//   id: NoteGroupID;
-//   title: string;
-// }
-
-interface NotesStore {
-  // groups: NoteGroup[];
-  notes: Note[];
-  // addGroup: () => void;
-  // deleteGroup: (id: NoteGroupID) => void;
-  addNote: () => void;
-  deleteNote: (id: NoteID) => void;
-  setNotes: (notes: Note[]) => void;
-  updateNoteTitle: (id: NoteID, title: string) => void;
-  updateNoteText: (id: NoteID, note: string) => void;
-  // save: () => void;
-  // _nextGroup: number;
-  _nextNote: number;
+interface UseNotesOptions {
+  onDeleteSuccess?: () => void;
 }
 
-export const useNotes = create<NotesStore>((set) => ({
-  // groups: [
-  //   { id: 1, title: "Group 1" },
-  //   { id: 2, title: "Group 2" },
-  // ],
-  notes: [],
-  // _nextGroup: 3,
-  _nextNote: 4,
+export const useNotes = ({ onDeleteSuccess }: UseNotesOptions) => {
+  const queryClient = useQueryClient();
 
-  setNotes: (notes: Note[]) => set(() => ({ notes })),
-  updateNoteTitle: (id, title) =>
-    set((state) => ({
-      notes: state.notes.map((note) =>
-        note.id !== id ? note : { ...note, title }
-      ),
-    })),
-  updateNoteText: (id, text) =>
-    set((state) => ({
-      notes: state.notes.map((note) =>
-        note.id !== id ? note : { ...note, note: text }
-      ),
-    })),
-  addNote: () => {
-    set((state) => ({
-      notes: [...state.notes, { id: state._nextNote, title: "", note: "" }],
-      _nextNote: state._nextNote + 1,
-    }));
-  },
-  deleteNote: (id: NoteID) =>
-    set((state) => ({
-      notes: state.notes.filter((note) => note.id !== id),
-    })),
-  save: () => {},
-}));
+  const updates = useRef(new Map<number, NoteContent>());
+  const updatesToCommit = useRef(new Map<number, NoteContent>());
+  const timerRef = useRef<number>(-1);
+
+  const { data: notes, isInitialLoading } = useQuery(
+    ["notes"],
+    () => getNotesAPI().then((res) => res.data as Note[]),
+    {
+      placeholderData: [],
+    }
+  );
+
+  const { isLoading: isAdding, mutate: addNote } = useMutation(
+    ["addNote"],
+    () => addNoteAPI().then((res) => res.data as Note),
+    {
+      onSuccess: (data) => {
+        queryClient.setQueryData<Note[]>(["notes"], (notes) =>
+          notes?.concat([data])
+        );
+      },
+    }
+  );
+
+  const { isLoading: isDeleting, mutate: deleteNote } = useMutation(
+    (id: NoteID) => deleteNoteAPI(id),
+    {
+      onSuccess: (_, id) => {
+        onDeleteSuccess?.();
+        queryClient.setQueryData<Note[]>(["notes"], (notes) =>
+          notes?.filter((note) => note.id !== id)
+        );
+      },
+    }
+  );
+
+  const { mutate: updateNotes } = useMutation(async () => {
+    applyUpdates();
+    return batchUpdate(updatesToCommit.current).then((res) => res.data);
+  });
+
+  const applyUpdates = useCallback(() => {
+    if (updates.current.size === 0) {
+      return;
+    }
+
+    updates.current.forEach((value, key) => {
+      const update = updatesToCommit.current.get(key);
+
+      if (!update) {
+        updatesToCommit.current.set(key, value);
+        return;
+      }
+
+      Object.assign(update, value);
+    });
+    updates.current.clear();
+  }, []);
+
+  const queueUpdate = useCallback(
+    (id: number, content: NoteContent) => {
+      clearTimeout(timerRef.current);
+
+      const update = updates.current.get(id);
+
+      if (!update) {
+        updates.current.set(id, content);
+      } else {
+        Object.assign(update, content);
+      }
+
+      console.log(updates.current);
+
+      timerRef.current = setTimeout(updateNotes, 3000);
+    },
+    [updateNotes]
+  );
+
+  return {
+    notes,
+    isInitialLoading,
+    isAdding,
+    addNote,
+    isDeleting,
+    deleteNote,
+    queueUpdate,
+  };
+};
